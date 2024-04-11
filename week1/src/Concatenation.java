204ccfacd7554233ac2c1e17133b523dc325fa88
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-04-11 15:45:53 -0700

    Add files via upload

---
+++ week1/src/Concatenation.java
@@ -0,0 +1,12 @@
+public class Concatenation {
+
+    public static void main(String[]args) {
+        String color = "red";
+        System.out.println("My favorite color is " +color);
+int money = 2_000_000;
+        System.out.println("I hit the jackpot and won $" +money+ " dallars!");
+        System.out.println(color+" "+money);
+        System.out.println("This will be my statement regarding my favorite color which is " +color+ " and will let let you know that I won a " +money+ " dollars." );
+        System.out.println(color + money);
+    }
+}
---
+++ week1/src/MyDataType.java
@@ -0,0 +1,25 @@
+public class MyDataType {
+
+    public static void main(String[] args) {
+        short number1 = 32_000;
+        int number5 = 2_000_000;
+        long number2 = 3_000_000_000L;
+        float number3 = 30.57F;
+        double number4 = 50.456373777;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+        boolean dry = true;
+
+        if(dry) {
+            System.out.println("Time to change up those curtains!!");
+        } else {
+                System.out.println("We will have to wait a spell");
+            }
+        }
+    }
+
