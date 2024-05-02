b12ad52fbda2ccd6e103f805987b2aaddf6b4aee
Ritz2020 <65273742+Ritz2020@users.noreply.github.com>
2024-05-02 10:53:00 -0700

    Add files via upload

---
+++ week4/src/MyPlayList.java
@@ -0,0 +1,11 @@
+public class MyPlayList {
+    public static void main(String[] args) {
+        int playlist = 1;
+
+        while( playlist <= 10) {
+
+            System.out.println("Playing my song!");
+            playlist++;
+        }
+    }
+}
